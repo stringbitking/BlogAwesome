94da92bea6beba1007714f9279f9de93df71ec81
stringbitking <victor.tsenkov@gmail.com>
2014-12-30 23:18:20 +0200

    Main layout, version 1.

---
+++ BlogAwesome/Scripts/app/app.js
@@ -0,0 +1,11 @@
+﻿var app = angular.module("BlogAwesome", ["kendo.directives"]);
+
+app.controller("NavigationController", function ($scope) {
+    $scope.menuOrientation = "horizontal";
+    $scope.onSelect = function (ev) {
+        console.log($(ev.item.firstChild).text());
+    };
+});
+
+
+
